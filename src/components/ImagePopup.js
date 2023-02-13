2cac9d67a20f67cd4b859435826dba423c25e34b
Selivokhina Anastasia <nastya.selivokhina8@gmail.com>
2023-02-13 22:44:44 +0300

    ref: добавлена деструктуризация в компонент ImagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,13 +1,14 @@
-function ImagePopup(props) {
+function ImagePopup({onOverlayClose, isOpen, onClose, card}) {
 
     return (
-        <div onClick={props.onOverlayClose} className={`popup popup_view-post ${props.isOpen ? 'popup_opened' : ''}`}>
+        <div onClick={onOverlayClose} className={`popup popup_view-post ${isOpen ? 'popup_opened' : ''}`}>
             <figure className="popup__img-container">
-                <button onClick={props.onClose} aria-label="Закрыть модальное окно" type="button" className="popup__close-button"></button>
+                <button onClick={onClose} aria-label="Закрыть модальное окно" type="button" className="popup__close-button"></button>
                 <img className="popup__image"
-                    src={props.card?.link ?? '#'}
-                    alt={props.card?.name ?? '#'} />
-                <figcaption className="popup__img-title">{props.card?.name ?? ''}</figcaption>
+                    src={card?.link ?? '#'}
+                    alt={card?.name ?? '#'}
+                />
+                <figcaption className="popup__img-title">{card?.name ?? ''}</figcaption>
             </figure>
         </div>
     )
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,6 +5,7 @@ function PopupWithForm(props) {
             <div className="popup__container">
                 <form name={props.name} noValidate className="popup__form">
                     <h2 className="popup__title">{props.title}</h2>
+                    {props.children}
                     <button type="submit" className="popup__submit popup__submit_delete">{props.submitText}</button>
                 </form>
                 <button aria-label="Закрыть модальное окно" type="button" className="popup__close-button" onClick={props.onClose}></button>
